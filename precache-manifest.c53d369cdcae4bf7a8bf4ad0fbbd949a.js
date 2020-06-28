cceb08007fdee72610b4c266ccc44e96725ba44d
Aleksandr Grigorii <domosedov.dev@gmail.com>
2020-06-28 19:05:25 +0300

    Updates

---
+++ precache-manifest.c53d369cdcae4bf7a8bf4ad0fbbd949a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0381e1d04c9ee0618cdef9515ae4553b",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "579ed18722ea0fcb520a",
+    "url": "/react-redux-todo/static/css/main.70c2a94d.chunk.css"
+  },
+  {
+    "revision": "389228f6a9b552cf016a",
+    "url": "/react-redux-todo/static/js/2.a38722cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-todo/static/js/2.a38722cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "579ed18722ea0fcb520a",
+    "url": "/react-redux-todo/static/js/main.3825b4d1.chunk.js"
+  },
+  {
+    "revision": "a86fe7d729ac95f8d5a8",
+    "url": "/react-redux-todo/static/js/runtime-main.e87a8b4b.js"
+  }
+]);
